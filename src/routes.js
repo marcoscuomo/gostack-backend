084acca1026844cb93b7026617f40528860f970c
bytecore@gmail.com <marcos@mojumob.com.br>
2019-10-29 16:07:41 -0300

    Criando SessionController

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,12 @@
 // import User from './app/models/User';
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 // Rota para teste
 // routes.get('/', async (req, res) => {
