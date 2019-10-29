8e6ff55e1e910da09b2a1bdc54c5cd7f34adc529
bytecore@gmail.com <marcos@mojumob.com.br>
2019-10-29 17:17:21 -0300

    Middleware de autenticacao

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,15 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 // Rota para teste
 // routes.get('/', async (req, res) => {
