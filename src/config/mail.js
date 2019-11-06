fe36260da49cd03e0a0a0200751fce65e9a7c831
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-05 23:03:27 -0300

    Configurando o Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '64002227d71c3f',
+    pass: '35475d67d4183a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
