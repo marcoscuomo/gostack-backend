fb20913c096d1d363e9c1a51ebb8adc2dec33ae6
bytecore@gmail.com <marcos@mojumob.com.br>
2019-11-08 00:10:14 -0300

    Configurando fila com Redis

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -5,7 +5,9 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
-import Mail from '../../lib/Mail';
+// import Mail from '../../lib/Mail';
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -143,19 +145,23 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      // text: 'Você tem um novo cancelamento',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
+
+    // await Mail.sendMail({
+    //   to: `${appointment.provider.name} <${appointment.provider.email}>`,
+    //   subject: 'Agendamento cancelado',
+    //   // text: 'Você tem um novo cancelamento',
+    //   template: 'cancellation',
+    //   context: {
+    //     provider: appointment.provider.name,
+    //     user: appointment.user.name,
+    //     date: format(appointment.date, "'dia' dd 'de' MMMM', às' H:mm'h'", {
+    //       locale: pt,
+    //     }),
+    //   },
+    // });
 
     return res.json(appointment);
   }
